aca62f6ada411f6964bb5c0310577aee71389d50
Andi Wijaya <andiwijayaid@gmail.com>
2018-11-30 17:45:29 +0800

    add more unit tests

--- app/src/test/java/com/example/andiwijaya/submission3/util/UtilsKtTest.kt
+++ app/src/test/java/com/example/andiwijaya/submission3/util/UtilsKtTest.kt
@@ -12,4 +12,28 @@ class UtilsKtTest {
             com.example.andiwijaya.submission3.util.replaceSemicolonWithNewRow("AA;BB;CC")
         )
     }
+
+    @Test
+    fun formatDateTimeToGMT() {
+        assertEquals(
+            "Sun Dec 02 01:30:00 SGT 2018",
+            com.example.andiwijaya.submission3.util.formatDateTimeToGMT("2018-12-01", "17:30:00+00:00")
+        )
+    }
+
+    @Test
+    fun getDateOnly() {
+        assertEquals(
+            "Sun, 02 Dec 2018",
+            com.example.andiwijaya.submission3.util.getDateOnly("Sun Dec 02 01:30:00 SGT 2018")
+        )
+    }
+
+    @Test
+    fun getTimeOnly() {
+        assertEquals(
+            "01:30",
+            com.example.andiwijaya.submission3.util.getTimeOnly("Sun Dec 02 01:30:00 SGT 2018")
+        )
+    }
 }
